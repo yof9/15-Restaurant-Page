35f2d982a536e7d04884271d87b3cacee0bfe73f
yof9 <149176377+yof9@users.noreply.github.com>
2025-03-26 17:02:45 +0300

    Feat-add home page layout
    added necessary dom elmts to html, styled with css, and created
    premlinary pseudocode in index.js.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import "./modern-normalize-custom.css";
+// import "modern-normalize";
+import "./modern-normalize-customized.css";
 import "./styles.css";
+
+// create main
+
+
+// add auto image swap functionality
+
+// add funcitionality for next and previous
+
+// create nav button hover effects
+
+// create menu
+
+// create about
